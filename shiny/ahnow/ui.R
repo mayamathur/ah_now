b90c9d39fe1cf5a9603b31d4c106843a3d0d6167
Maya Mathur <mmathur@stanford.edu>
2018-06-25 13:20:35 -0400

    Init Shiny app

---
+++ shiny/ahnow/ui.R
@@ -0,0 +1,34 @@
+
+
+source("startup.R")
+
+
+# message to display if non-null true value
+nonnull.mess = 'Note: You are calculating a "non-null" E-value, i.e., an E-value for the minimum
+                amount of unmeasured confounding needed to move the estimate and confidence interval
+                to your specified true value rather than to the null value.'
+
+
+fluidPage( title = "E-value calculator",
+            
+                      mainPanel(
+                        
+                          wellPanel(  HTML(paste("<b>We've moved!</b>",
+                        
+                                      "The E-value calculator <a href='https://evalue.hmdc.harvard.edu/app/'>has moved</a>.",
+                                      sep="<br/><br/>")) ),
+
+                          width=6
+                          
+                      )
+)
+                      
+                     
+
+
+
+
+
+
+
+
